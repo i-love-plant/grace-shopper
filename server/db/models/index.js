5d4cb2706a573d7bddbeed3d531a4351890b0327
Hannah Weber <hkwweber@gmail.com>
2018-01-09 15:14:25 -0500

    added model assocations and orderItem model

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -1,11 +1,33 @@
 const User = require('./user')
 const Product = require('./product')
+const OrderItem = require('./orderItem')
+const Category = require('./category');
+const Order = require('./order');
+const Review = require('./review');
 /**
  * If we had any associations to make, this would be a great place to put them!
  * ex. if we had another model called BlogPost, we might say:
  *
  *    BlogPost.belongsTo(User)
  */
+
+ Product.belongsToMany(Category, {through: 'productCategory'});
+ Category.belongsToMany(Product, {through: 'productCategory'});
+
+ User.hasMany(Order);
+ Order.belongsTo(User);
+
+ User.hasMany(Review);
+ Review.belongsTo(User);
+
+ Product.hasMany(Review);
+ Review.belongsTo(Product);
+
+ OrderItem.belongsTo(Product);
+ Product.hasMany(OrderItem);
+
+ OrderItem.belongsTo(Order);
+ Order.hasMany(OrderItem);
 
 /**
  * We'll export all of our models here, so that any time a module needs a model,
@@ -15,5 +37,6 @@ const Product = require('./product')
  */
 module.exports = {
   User,
-  Product
+  Product,
+  OrderItem
 }
