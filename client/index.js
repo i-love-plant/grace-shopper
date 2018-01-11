9429ddd0c2e91d994434cb8ee0d050b0263d628f
Ann Layman <annelayman@gmail.com>
2018-01-11 17:15:29 -0500

    restructuring files, made UserInterface component

--- client/index.js
+++ client/index.js
@@ -2,14 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import {Provider} from 'react-redux'
 import store from './store'
-import Routes from './routes'
+import UserInterface from './components'
 
 // establishes socket connection
 import './socket'
 
 ReactDOM.render(
   <Provider store={store}>
-    <Routes />
+    <UserInterface />
   </Provider>,
   document.getElementById('app')
 )
