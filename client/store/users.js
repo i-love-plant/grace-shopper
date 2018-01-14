975e2e26a8c08b69ec11aab294d63020d597f51f
Katharine Chun <katharine.chun@nyu.edu>
2018-01-14 16:45:50 -0500

    added /users/:id api route and frontend route. frontend route users/:userid routes to ViewUser component. this route and component are for admin use only

--- client/store/users.js
+++ client/store/users.js
@@ -5,18 +5,21 @@ import history from '../history'
  * ACTION TYPES
  */
 const GET_USERS = 'GET_USERS'
+const GET_SINGLE_USER = 'GET_SINGLE_USER'
 
 /**
  * INITIAL STATE
  */
 const initialState = {
-    users: []
+    users: [],
+    currentUser: {}
 }
 
 /**
  * ACTION CREATORS
  */
 const getUsers = users => ({ type: GET_USERS, users })
+const getSingleUser = user => ({ type: GET_SINGLE_USER, user });
 
 /**
  * THUNK CREATORS
@@ -33,14 +36,27 @@ export function fetchUsers() {
     };
 }
 
+export function fetchUser(userId) {
+    return function thunk(dispatch) {
+        return axios.get(`/api/users/${userId}`)
+            .then(res => res.data)
+            .then(user => {
+                const action = getSingleUser(user);
+                dispatch(action);
+            })
+            .catch(error => console.log(error));
+    };
+}
+
 /**
  * REDUCER
  */
 export default function (state = initialState, action) {
   switch (action.type) {
     case GET_USERS:
-      //return action.users
       return Object.assign({}, state, { users: action.users });
+    case GET_SINGLE_USER:
+      return Object.assign({}, state, { currentUser: action.user })
     default:
       return state
   }
