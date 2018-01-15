e7893cf564a7c5d57b7a87ae798fc1bcd1a118ee
Ann Layman <annelayman@gmail.com>
2018-01-14 22:43:46 -0500

    styled the many products page and started on single product

--- client/components/ManyProducts.js
+++ client/components/ManyProducts.js
@@ -12,62 +12,45 @@ const ManyProducts = (props) => {
 
     return (
         <div>
-            <h3>Products: </h3>
-            <SearchBar />
-            <div className="search-control">
-                <select defaultValue={categories.id} name="category" onChange={props.handleCategoryChange}>
-                    <option value="-1">Select A Category</option>
-                    {
-                        categories.map(category => {
-                            return (
-                                <option key={category.id} value={category.id}>{category.name}
-                                </option>
-                            )
-                        })
-                    }
-                </select>
+            <h1>Lovely Plants For Purchase</h1>
+            <div id="search-controls">
+                <SearchBar />
+                <div className="search-control">
+                    <select id="category-select" defaultValue={categories.id} name="category" onChange={props.handleCategoryChange}>
+                        <option value="-1">Select A Category</option>
+                        {
+                            categories.map(category => {
+                                return (
+                                    <option key={category.id} value={category.id}>{category.name}
+                                    </option>
+                                )
+                            })
+                        }
+                    </select>
+                </div>
             </div>
 
-            <table id="products-table">
-                <tbody>
-                    <tr>
-                        <td>Name</td>
-                        <td>Description</td>
-                        <td>Image</td>
-                        <td>Price</td>
-                        <td>Category</td>
-                    </tr>
-                    {
-                        props.products.map(product => {
-                            return (
-                                <tr key={product.id}>
-                                    <td><Link to={`/products/${product.id}`}>{product.name}</Link></td>
-                                    <td>{product.description}</td>
-                                    <td>
-                                        <img src={product.image} className="product-image" />
-                                    </td>
-                                    <td>{product.price}</td>
-                                    <td>
-                                        <ul>
-                                            {product.categories.map(category => {
-                                                return (
-                                                    <li key={category.id}>
-                                                        {category.name}
-                                                    </li>
-                                                )
-                                            })}
-                                        </ul>
-                                    </td>
-                                </tr>
-                            )
-                        })
-                    }
-                </tbody>
+            <div id="products-grid">
+                {
+                    props.products.map(product => {
+                        return (
+                            <div key={product.id} className="product-square">
+                                <div>
+                                    <img src={product.image} className="product-image" />
+                                </div>
+                                <Link to={`/products/${product.id}`}>{product.name}</Link>
+                                <div className="product-price">${product.price}
+                                </div>
+                            </div>
+                        );
+                    })
+                }
+            </div>
 
-            </table>
+
         </div>
-    )
-}
+    );
+};
 
 /**
  * CONTAINER
