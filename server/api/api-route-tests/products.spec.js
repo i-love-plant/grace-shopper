a6640a17b08d07add489b71fb1f84d33993e208d
Hannah Weber <hkwweber@gmail.com>
2018-01-15 17:00:32 -0500

    added test folders

--- server/api/api-route-tests/products.spec.js
+++ server/api/api-route-tests/products.spec.js
@@ -2,8 +2,8 @@
 
 const { expect } = require("chai");
 const request = require("supertest");
-const db = require("../db");
-const app = require("../index");
+const db = require("../../db");
+const app = require("../../index");
 const Product = db.model("product");
 
 describe("Product routes", () => {
